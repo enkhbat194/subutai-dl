e8d9026a13cd5896fbb18f620ce18cffce22dd4a
enkhbat194 <34760209+enkhbat194@users.noreply.github.com>
2026-08-02 00:01:20 +0800

    Mount batch download interface

--- apps/desktop/src/renderer/src/RootApp.tsx
+++ apps/desktop/src/renderer/src/RootApp.tsx
@@ -1,4 +1,5 @@
 import type { ReactElement } from 'react';
+import { BatchDownloadLauncher } from './BatchDownloadLauncher';
 import { MediaLauncher } from './MediaLauncher';
 import { QueueSchedulerLauncher } from './QueueSchedulerLauncher';
 import { SubutaiApp } from './SubutaiApp';
@@ -8,6 +9,7 @@ export function RootApp(): ReactElement {
   return (
     <>
       <SubutaiApp />
+      <BatchDownloadLauncher />
       <TransferSettingsLauncher />
       <QueueSchedulerLauncher />
       <MediaLauncher />
