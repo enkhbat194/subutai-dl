137027cfd38c5a5fe93c2f0b579dcd80dcb4d34a
enkhbat194 <34760209+enkhbat194@users.noreply.github.com>
2026-08-23 03:20:55 +0800

    Update packaged yt-dlp to current stable

--- scripts/install-temporary-media-tools.ps1
+++ scripts/install-temporary-media-tools.ps1
@@ -4,9 +4,9 @@ param(
 
 $ErrorActionPreference = "Stop"
 
-$ytDlpVersion = "2026.06.09"
+$ytDlpVersion = "2026.07.04"
 $ytDlpUrl = "https://github.com/yt-dlp/yt-dlp/releases/download/$ytDlpVersion/yt-dlp.exe"
-$ytDlpSha256 = "3a48cb955d55c8821b60ccbdbbc6f61bc958f2f3d3b7ad5eaf3d83a543293a27"
+$ytDlpSha256 = "52fe3c26dcf71fbdc85b528589020bb0b8e383155cfa81b64dd447bbe35e24b8"
 
 $ffmpegBuild = "ffmpeg-N-123778-g3b55818764-win64-gpl"
 $ffmpegUrl = "https://github.com/yt-dlp/FFmpeg-Builds/releases/download/autobuild-2026-03-31-15-13/$ffmpegBuild.zip"
